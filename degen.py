e90d33308c67f7d922cd26f2cf6ad9d38e92462b
Yannick Roy <ymyannick@gmail.com>
2023-11-05 18:44:37 +0200

    Temp debug stuff. To be removed.

--- degen.py
+++ degen.py
@@ -124,35 +124,20 @@ def plot_chart(price_df, liquidation_price=None):
 
 
 @st.cache_data
-def fetch_prices(token, start_time, end_time=None, interval='1h'):
-    pair = token+'USDT'
-    binance_url = "https://api.binance.com/api/v3/klines?symbol={}&interval={}&startTime={}&endTime={}&limit=1000".format(
-        pair, interval, str(start_time*1000), str(end_time*1000))
-    print(binance_url)
-    st.write(binance_url)
+def fetch_prices(token, start_time, end_time=None, interval='1h', source='Binance'):
+    if source == 'Binance':
+        pair = token+'USDT'
+        binance_url = "https://api.binance.us/api/v3/klines?symbol={}&interval={}&startTime={}&endTime={}&limit=1000".format(
+            pair, interval, str(start_time*1000), str(end_time*1000))
+        print(binance_url)
+        st.write(binance_url)
 
-    price_response = ast.literal_eval(requests.get(binance_url).text)
-    price_df = pd.DataFrame(price_response, columns=['time', 'open', 'high', 'low', 'close', 'volume', 'date', 'asset_vol', 'txs', 'irr', 'irr2', 'irr3'])
-    price_df = price_df[['time', 'open', 'high', 'low', 'close', 'volume']]
-    price_df['time'] = price_df['time'].apply(lambda x: int(x/1000))
-
-    return price_df
-
-
-def fetch_prices2(token, start_time, end_time=None, interval='1h'):
-    pair = token+'USDT'
-    binance_url = "https://api.binance.com/api/v3/klines?symbol={}&interval={}&startTime={}&endTime={}&limit=1000".format(
-        pair, interval, str(start_time*1000), str(end_time*1000))
-    print(binance_url)
-    st.write(binance_url)
-    
-    response = requests.get(binance_url).text
-    st.write(response)
-    price_response = ast.literal_eval(response)
-    st.write(price_response)
-    price_df = pd.DataFrame(price_response, columns=['time', 'open', 'high', 'low', 'close', 'volume', 'date', 'asset_vol', 'txs', 'irr', 'irr2', 'irr3'])
-    price_df = price_df[['time', 'open', 'high', 'low', 'close', 'volume']]
-    price_df['time'] = price_df['time'].apply(lambda x: int(x/1000))
+        price_response = ast.literal_eval(requests.get(binance_url).text)
+        price_df = pd.DataFrame(price_response, columns=['time', 'open', 'high', 'low', 'close', 'volume', 'date', 'asset_vol', 'txs', 'irr', 'irr2', 'irr3'])
+        price_df = price_df[['time', 'open', 'high', 'low', 'close', 'volume']]
+        price_df['time'] = price_df['time'].apply(lambda x: int(x/1000))
+    elif source == 'Coingecko':
+        st.write('Working on it.')
 
     return price_df
 
@@ -185,6 +170,11 @@ def calculate_liquidation_price(entry_price, leverage, liquidation_limit=0.95, i
 # ==========================================================================================
 st.set_page_config(layout="wide")
 st.header("🦍 Degen Together Strong.")
+
+st.sidebar.markdown('## Degen Config')
+
+# Sidebar
+prices_source = st.sidebar.radio('Prices Source:', ['Binance', 'Goingecko'], horizontal=True)
 
 # Symbol selection
 selected_crypto_fullname = st.sidebar.selectbox('Select Token:', list(TOKEN_MAPPING))
@@ -272,9 +262,6 @@ expand_todo.markdown('- [ ] Sweep Tokens')
 expand_todo.markdown('- [ ] Report')
 if expand_todo.button('Clear Cache 👍'):
     st.cache_data.clear()
-if expand_todo.button('Fetch no Cache'):
-    df_prices2 = fetch_prices2(selected_token, start_timestamp, end_timestamp, interval=interval)
-    st.write(df_prices2)
 
 st.sidebar.markdown('---')
 st.sidebar.markdown('`May the gains be with you. 🚀 🌕`')
